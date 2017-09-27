d4e20da0e40b57c3a5242cfccd5a0c701549688f
ScottKersevan <32210664+ScottKersevan@users.noreply.github.com>
2017-09-27 19:02:32 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! From, Scott Kersevan. " +
+                "The current date and time is" string.Format );
+        
+            var time = DateTime.Now;
+
+            Console.WriteLine(time);
+        }
+    }
+}
